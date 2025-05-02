9fee118b45d7bc2c19f12c7ea115b9cc0361bbf4
balakrishna <kulalbalakrishna4@gmail.com>
2025-05-02 16:42:09 +0530

    nwotst

--- index.js
+++ index.js
@@ -44,13 +44,16 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
 }))
-app.use(express.json())
-app.use(urlencoded({ extended: true }))
+
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST'],
   credentials: true,
 }))
+
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
+
 app.use(passport.initialize());
 app.use(passport.session());
 
