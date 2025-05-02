77705cfb1d022386df2f83c7db7a48b53791eaf4
balakrishna <kulalbalakrishna4@gmail.com>
2025-05-02 15:44:04 +0530

    nwotst

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get("/", async(req, res) => {
 app.get("/login", (req, res) => {
   res.status(200).json({
         success: true,
-        user: req.user
+        user: req.user,
       })
   // if(req.user) {
   //   res.status(200).json({
