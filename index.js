3f8d4849cb41f784f7cc613f86b5dcc47fedb090
balakrishna <kulalbalakrishna4@gmail.com>
2025-05-02 14:38:05 +0530

    no commit

--- index.js
+++ index.js
@@ -185,7 +185,9 @@ app.post("/login", (req,res, next) => {
       
       res.redirect(origin_url+'Login');
     };
-  })(req, res, next);
+  },(req,res)=>{
+    req.session.user = req.user;
+ })(req, res, next);
 })
 
 app.get("/auth/google",
