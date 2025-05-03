d6acd862006d8e25717ef929c05e5eca4fc61a5d
balakrishna <kulalbalakrishna4@gmail.com>
2025-05-03 14:09:53 +0530

    final commit18

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: true,
-    httpOnly: false,
+    secure: false,
+    httpOnly: true,
     sameSite: 'lax'
   }
 }))
